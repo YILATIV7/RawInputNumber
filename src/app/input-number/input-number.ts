dce4acf99843e666f0d1bac54918b1531bfffbd3
Vitalii <vitalytyrenko7@gmail.com>
2025-06-01 20:43:59 +0300

    Init commit

--- src/app/input-number/input-number.ts
+++ src/app/input-number/input-number.ts
@@ -1,25 +1,116 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+
+interface InputState {
+    value: string;
+    cursorIndex: number;
+}
 
 @Component({
-  selector: 'app-input-number',
-  imports: [],
-  templateUrl: './input-number.html',
-  styleUrl: './input-number.css'
+    selector: 'app-input-number',
+    imports: [],
+    templateUrl: './input-number.html',
+    styleUrl: './input-number.css'
 })
 export class InputNumber implements OnInit {
-  value: string = "";
+    @Input() fractional: number = 4;
 
-  constructor(
+    @ViewChild('numberInput')
+    set dateInputRef(value: ElementRef<HTMLInputElement> | undefined) {
+        this.element = value?.nativeElement;
+    }
 
-  ) {
-  }
+    element: HTMLInputElement | undefined;
 
-  ngOnInit() {
+    constructor() {
+    }
 
-  }
+    ngOnInit() {
 
-  proceedInput(event: Event) {
-    console.log(event);
-    console.log(typeof event);
-  }
+    }
+
+    public onBeforeInput(event: InputEvent) {
+        if (!this.element) return;
+        event.preventDefault();
+
+        const cursorStart = this.element.selectionStart!;
+        const cursorEnd = this.element.selectionEnd!;
+        const valStr = this.element.value;
+
+        if (event.inputType === "deleteContentBackward") {
+            if (cursorStart === cursorEnd) {
+                this.element.value = this.element.value.substring(0, cursorStart - 1) + this.element.value.substring(cursorStart);
+                this.element.setSelectionRange(cursorStart - 1, cursorStart - 1);
+            } else {
+                this.element.value = this.element.value.substring(0, cursorStart) + this.element.value.substring(cursorEnd);
+                this.element.setSelectionRange(cursorStart, cursorStart);
+            }
+
+        } else if (event.inputType === "deleteContentForward") {
+            // do nothing
+
+        } else if (event.inputType === "insertText" && cursorStart === cursorEnd) {
+            const prevState: InputState = {
+                value: this.element.value,
+                cursorIndex: this.element.selectionStart!,
+            }
+
+            if (event.data && event.data.length === 1) {
+                const resultState: InputState = this.processSymbol(event.data, prevState);
+                this.element.value = resultState.value;
+                this.element.setSelectionRange(resultState.cursorIndex, resultState.cursorIndex);
+            }
+        }
+    }
+
+    private processSymbol(symbol: string, state: InputState) {
+
+        if (this.isSeparator(symbol)) {
+            if (!state.value.includes('.')) {
+                return {
+                    value: state.value.substring(0, state.cursorIndex) + '.' + state.value.substring(state.cursorIndex),
+                    cursorIndex: state.cursorIndex + 1,
+                };
+            }
+            return state;
+        }
+
+        if (symbol === '-') {
+            return state;
+        }
+
+        if (!this.isDigit(symbol)) return state;
+
+        const separatorIndex = state.value.indexOf('.');
+        const [intPart, fracPart] = state.value.split('.');
+
+        if (state.cursorIndex <= separatorIndex) {
+            // курсор до розділювача
+            return {
+                value: state.value.substring(0, state.cursorIndex) + symbol + state.value.substring(state.cursorIndex),
+                cursorIndex: state.cursorIndex + 1,
+            };
+
+        } else {
+            // курсор після розділювача
+            if (fracPart && fracPart.length < this.fractional) {
+                return {
+                    value: state.value.substring(0, state.cursorIndex) + symbol + state.value.substring(state.cursorIndex),
+                    cursorIndex: state.cursorIndex + 1,
+                };
+            } else {
+                return {
+                    value: state.value.substring(0, state.cursorIndex) + symbol + state.value.substring(state.cursorIndex + 1),
+                    cursorIndex: state.cursorIndex + 1,
+                };
+            }
+        }
+    }
+
+    private isSeparator(char: string): boolean {
+        return ['.', ',', 'б', 'ю', '/', '?'].includes(char.toLowerCase());
+    }
+
+    private isDigit(char: string): boolean {
+        return /^\d$/.test(char);
+    }
 }
